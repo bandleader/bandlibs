7ba615828055d255d4df82c273f198aa7ccb445f
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-01-24 12:59:19 -0500

    vue-fiddle-helper: new version

---
+++ vue-fiddle-helper/for-script-tag.ts
@@ -0,0 +1,5 @@
+import * as Vue from '../../TableSeater4/frontend/node_modules/vue/dist/vue.esm-browser'
+import { initApp } from './main-helper'
+;(window as any).Vue = Vue
+initApp(Vue)
+
--- vue-fiddle-helper/main-helper.ts
+++ vue-fiddle-helper/main-helper.ts
@@ -1,23 +1,7 @@
+import * as VCP from '../vue-class-plus'
+import * as Vug from '../vug'
 
-function _createVueHelper(appOpts: any) {
-    const hooks: Function[] = []
-    const hook = (fn: (app: any) => void) => { hooks.push(fn) }
-    const comp = (name: string, obj: any) => hook(x => x.component(name, vueClassPlus(obj)))
-    // Initialize the app on next tick so that all the components are registered
-    const w = window as any
-    const Vue = w.Vue
-    let vm = Vue.createApp(appOpts)
-    w.vm = vm
-    setTimeout(() => {
-        for (const h of hooks) h(vm)
-        vm.mount('#app')
-    })
-    return { hook, comp, vm, Vue }
-}
-export function VueHelper(): ReturnType<typeof _createVueHelper>
-export function VueHelper(componentName: string, optsOrClass: any): ReturnType<typeof _createVueHelper>
-export function VueHelper(a?: any, b?: any): ReturnType<typeof _createVueHelper> {
-    // Create a singleton instance
+export function initApp(Vue = (window as any).Vue) {
     const w: any = window
     const mainInstance = {
         data: () => ({
@@ -30,8 +14,49 @@ export function VueHelper(a?: any, b?: any): ReturnType<typeof _createVueHelper>
             })
         }
     }
-    const helper: ReturnType<typeof _createVueHelper> = w.vueHelper = w.vueHelper || _createVueHelper(mainInstance)
-    // Create component if that overload was used
-    if (a) helper.comp(a, b)
-    return helper
+    const div = document.createElement("div")
+    document.body.appendChild(div)
+    w.app = Vue.createApp(mainInstance)
+    
+    // Support vue-class-plus
+    const hook = (obj: any, prop: string, fn: Function) => { const old = obj[prop].bind(obj); obj[prop] = (...args: any[]) => fn.apply(obj, [old]).apply(obj, args) }
+    hook(w.app, 'component', (old: Function) => (name: string, value: any, ...args: any[]) => old(name, VCP.vueClassPlus(value), ...args))
+
+    // Support vug
+    hook(w.app, 'component', (old: Function) => (name: string, value: any, ...args: any[]) => {
+        if (value.vug) { value.template = Vug.load(value.vug).toVueTemplate(); delete value.vug }
+    })
+    
+    // Include two components
+    w.app.component("async-value", class { 
+        promise = VCP.propRequired<Promise<any>>()
+        resolved = false
+        value: any = null
+        error: any = null
+        created() { 
+            this.promise.then(x => { this.value = x; this.resolved = true }, err => this.error = err)
+        }            
+        static template = `<slot v-if="resolved" v-bind="{value}" /><span v-else-if="error" class="text-danger"><i class="fa fa-exclamation-triangle" /> {{String(error)}}</span><span v-else class="text-primary spinner-border spinner-border-sm" role="status"></span>`
+    })
+    w.app.component("promise-button", class {
+        action = VCP.propRequired<((ev: Event) => Promise<any>)>()
+        pending = false
+        success = false
+        error: any = null
+        errorClicked() { 
+            alert(this.error) 
+        }
+        go(ev: Event) {
+            this.pending = true
+            this.error = null
+            this.success = false
+            this.action(ev).then(x => {
+                this.pending = false
+                this.success = true
+                setTimeout(() => this.success = false, 2000)
+            }, e => { this.pending = false; this.error = e })
+        }
+        static template = `<button @click="go" :disabled="pending"><slot /><transition name="fade"><span v-if="pending||error||success"><div v-if="pending" class="spinner-border spinner-border-sm ms-2" style="font-size: 0.7em" role="status" /><span v-else-if="error" :title="String(error)" @click.stop="errorClicked">⚠</span><span v-else-if="success">✅</span></span></transition></button>`
+    })
+    setTimeout(() => w.app.mount(div)) // Mount on next tick so all the components are ready
 }
