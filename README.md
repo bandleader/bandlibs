4c76613ad7c02abd81a35a206f3fb925b5561dee
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-01-27 20:53:03 -0500

    add 'reveal'

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ A collection of small utilities, mostly for Javascript/Typescript/Vue, that I wo
 - **[vue-fiddle-helper](vue-fiddle-helper/)** -- reduced boilerplate for quick Vue fiddles. Includes navbar, routing, vue-promise-button, vue-async-value, vue-class-plus, Bootstrap, FontAwesome
 - **[vue-promise-button](vue-promise-button/)** -- button component for Vue that shows feedback while its action is pending
 - **[vue-async-value](vue-async-value/)** -- component for easy consumption of async data or async computed properties in Vue templates
+- **[reveal](reveal/)** -- animate HTML elements when they come into view (including easy Vue directive)
 - **[utils](utils/)** -- miscallaneous helper functions; might not even be worth the dependency, but you can copy them into your projects
 
 ### Libraries I hope to post here soon:
 - **[ferry](ferry/)** -- WIP -- two-way-sync arbitrary Javascript objects -- changes are streamed as patches which the other side applies
-- **reveal** -- animate HTML elements when they come into view (including easy Vue directive)
 - **node-quick-control** -- a script to easily control a server. Even easier if you run through npx
 - **roll-your-lib** -- one line to compile your Typescript library for use as a CommonJS module, an ES6 module, or via a script tag. Designed for running as an npm script or via npx
 - **vue-item-list** -- Vue component to show a list of items, allow selecting them, and perform actions on them
---
+++ reveal/index.ts
@@ -0,0 +1,40 @@
+export function isVisible(el: HTMLElement, partial = true) {
+    const viewTop = window.scrollY,
+        viewBottom = viewTop + window.innerHeight,
+        _top = el.offsetTop,
+        _bottom = _top + el.offsetHeight,
+        compareTop = partial ? _bottom : _top,
+        compareBottom = partial ? _top : _bottom;
+    return ((compareBottom <= viewBottom) && (compareTop >= viewTop))
+  }
+  
+  export default function reveal(els: HTMLElement[], effect?: object, speed = 500, staggerChildren = 500, delay = 0) {
+    effect = effect || { opacity: 0 }
+    const hide = (el: HTMLElement) => Object.assign(el.style, effect)
+    const reveal = (el: HTMLElement) => { const oldTransition = el.style.transition; el.style.transition = `all ${speed}ms`; Object.keys(effect).forEach(k => el.style[k] = ''); setTimeout(() => el.style.transition = oldTransition, 1000) }
+    const revealAll = () => els.forEach((x,i) => setTimeout(() => reveal(x), i * staggerChildren))
+    let done = false
+    const test = () => isVisible(els[0])
+    const check = () => { if (done || !test()) return; done = true; setTimeout(revealAll, delay) }
+    els.forEach(hide)
+    setTimeout(check, 5)
+    if (!done) window.addEventListener('scroll', check)
+  }
+  
+  export const vueDirective = {
+    mounted(el: HTMLElement, {value, modifiers}: {value: any, modifiers: any}) { 
+      if (isVisible(el) && modifiers.noimm) return;
+      const els = modifiers.children ? (Array.from(el.children) as HTMLElement[]) : [el]
+      const getKeyArg = (prefix: string) => {
+        const findKey = Object.keys(modifiers).find(x => x.startsWith(prefix + "-"))
+        if (!findKey) return null
+        return findKey.slice(prefix.length + 1)
+      }
+      const parseSpeed = (str: string) => { let ms = parseFloat(str); return ms < 20 ? ms * 1000 : ms }
+      const staggerChildren = getKeyArg('stagger') ? parseSpeed(getKeyArg('stagger')) : undefined
+      const delay = getKeyArg('delay') ? parseSpeed(getKeyArg('delay')!) : undefined
+      const speed = getKeyArg('speed') ? parseSpeed(getKeyArg('speed')!) : undefined
+      reveal(els, value, speed, staggerChildren, delay)
+    } 
+  }
+  
