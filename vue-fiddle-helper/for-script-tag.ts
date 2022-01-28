c09bfaee9342159572c89ac51ed7837a5e444cce
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-01-27 20:19:18 -0500

    rename fiddlehelper to index

--- vue-fiddle-helper/for-script-tag.ts
+++ vue-fiddle-helper/for-script-tag.ts
@@ -1,5 +1,5 @@
 import * as Vue from 'vue/dist/vue.esm-browser'
-import { initApp } from './main-helper'
+import { initApp } from './'
 import * as VCP from '../vue-class-plus'
 import * as Vug from '../vug'
 
