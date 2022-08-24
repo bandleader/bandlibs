0b1a45ef93998d4b0bd6da852fd9b30b8eaeadee
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-08-24 21:19:14 +0300

    VugDocs use new Markdown features -- EXPERIMENTALLY

--- site/src/components/VugDocs.vue
+++ site/src/components/VugDocs.vue
@@ -54,24 +54,24 @@ section.py-4 bg=#AAA
 .container.py-4#documentation
   css:[& aside] font-style=italic c=#777 d=inline mx=1q
   # Documentation
-  | Vug is a concise language for expressing DOM elements, inspired by [Pug](https://pugjs.org), [Imba Elements](https://imba.io/docs/tags), and [Tailwind CSS](https://tailwindcss.com).
+  Vug is a concise language for expressing DOM elements, inspired by [Pug](https://pugjs.org), [Imba Elements](https://imba.io/docs/tags), and [Tailwind CSS](https://tailwindcss.com).
   - Saves you **plenty** of time
   - Works well
   
-  h3 -- Basics
+  ### Basics
 
-  p -- Each element goes on its own line, and consists of the tag name, optionally followed by <code> -- </code> and any text content:
+  Each element goes on its own line, and consists of the tag name, optionally followed by <code> -- </code> and any text content:
   VugDocsExample.mb-4 > pre
     -- h3 -- Welcome to Vug!
     -- p -- Let's describe Vug in a paragraph.
   
-  p -- You can add comments after a <code>//&nbsp;</code>, like in Javascript:
+  You can add comments after a <code>//&nbsp;</code>, like in Javascript:
     aside -- (make sure there's a space after the <code>//</code> -- this helps to disambiguate from things like <code>https://example.com</code>)
   VugDocsExample.mb-4 code=(commentExampleCode)
 
 
 
-  p -- You can create nested structures of elements simply by indenting them appropriately:
+  You can create nested structures of elements simply by indenting them appropriately:
   VugDocsExample.mb-4 > pre
     -- div
     --   p -- This paragraph is inside the div.
@@ -81,95 +81,91 @@ section.py-4 bg=#AAA
     --   -- Here's some plain text inside the paragraph,
     --   -- and another line of plain text.
 
-  h3 -- Classes and IDs
+  ### Classes and IDs
   
-  p -- You can add CSS classes to to the tag name using the <code>.</code> character, just like in CSS itself:
+  You can add CSS classes to to the tag name using the `.` character, just like in CSS itself:
   VugDocsExample.mb-4 > pre -- div.fancy -- Hello there
-  p -- Multiple classes work too:
+  Multiple classes work too:
   VugDocsExample.mb-4 > pre -- button.btn.btn-primary -- Save
-  p -- Similarly, you can set the ID with the <code>#</code> character:
+  Similarly, you can set the ID with the `#` character:
   VugDocsExample.mb-4 > pre -- div.bold#customerName -- ACME Industries
-  p -- If the tag is <code>div</code>, you can just write classes and/or IDs, and skip the tag name:
+  If the tag is `div`, you can just write classes and/or IDs, and skip the tag name:
   VugDocsExample.mb-4 > pre 
     -- .row
     --   .col-3 
     --   .col-9#mainColumn 
     -- #footer
 
-  h3 -- Attributes and Inline Styles
-  p -- You can set attributes on elements just like in HTML:
+  ### Attributes and Inline Styles
+  You can set attributes on elements just like in HTML:
   VugDocsExample.mb-4 > pre -- input type="text" value="Hello there"
-  p -- However, if the attribute value does not contain any spaces, you can skip the quotes
+  However, if the attribute value does not contain any spaces, you can skip the quotes
     aside -- (HTML also supports this, but in Vug, it fits in well with the language and is the idiomatic style):
   VugDocsExample.mb-4 > pre -- input type=text value="Multiple words still need quotes"
-  p -- Like in HTML, you can specify an attribute without a value, which is equivalent to setting it to <code>&quot;&quot;</code>:
+  Like in HTML, you can specify an attribute without a value, which is equivalent to setting it to `""`:
   VugDocsExample.mb-4 > pre -- input type=checkbox checked
-  p -- You can also set CSS attributes directly on the element. In HTML, you are probably used to doing this in a <code>style</code> attribute:
+  You can also set CSS attributes directly on the element. In HTML, you are probably used to doing this in a `style` attribute:
   VugDocsExample.mb-4 > pre -- div style="font-size: 1.5em; font-weight: bold;"
-  p -- But in Vug, we recognize all CSS property names, so you can set them directly on the element:
+  But in Vug, we recognize all CSS property names, so you can set them directly on the element:
   VugDocsExample.mb-4 > pre -- div font-size=1.5em font-weight=bold
   // TODO link to shorthand etc, and to binding
 
-  h3 -- CSS Shorthand
+  ### CSS Shorthand
   // TODO
 
-  h3 -- CSS Macros
+  ### CSS Macros
 
-  h3 -- Custom Tag Types
-  p -- Including flex macros or arg
+  ### Custom Tag Types
+  Including flex macros or arg
 
-  h3
-    -- Markdown
+  ### Markdown
     s.badge.rounded-pill.bg-danger fs=0.4em -- EXPERIMENTAL
   
-  p -- Being that Vug is all about conciseness, it seems natural to support Markdown directly in Vue templates. This is useful for text-heavy pages.
+  Being that Vug is all about conciseness, it seems natural to support Markdown directly in Vue templates. This is useful for text-heavy pages.
   VugDocsExample.mb-4 > pre
-    -- flex:c.c bg=#222 p=5em mb=1em
-    --   .card > .card-body
-    --     # Heading 1
-    --     ## Heading 2
-    --     ### Heading 3
-    --     #### Heading 4
-    --     ##### Heading 5
-    --     ###### Heading 6
-    --     
-    --     | _Regular_ paragraphs of text must start with the pipe (`|`) character.
-    --     | Otherwise, they would be confused with regular elements.
-    --     | Each line is a new paragraph.
+    raw -- flex:c.c bg=#222 p=5em mb=1em
+    raw --   .card > .card-body
+    raw --     # Heading 1
+    raw --     ## Heading 2
+    raw --     ### Heading 3
+    raw --     #### Heading 4
+    raw --     ##### Heading 5
+    raw --     ###### Heading 6
+    raw --     
+    raw --     | _Regular_ paragraphs of text must start with the pipe (`|`) character.
+    raw --     | Otherwise, they would be confused with regular elements.
+    raw --     | Each line is a new paragraph.
+    
+    raw -- - You can of course use [Markdown Syntax](https://www.markdownguide.org/basic-syntax/) inside these lines. **Bold**, _italics_, [links](#), etc.
+    raw -- - Unordered lists work too.
+    raw --   1. As well as ordered ones.
+    raw --   2. Indentation works the usual way.
+    raw --   3. Everything should work.
+    raw --   // - Starting a line with <code>#</code>, <code>##</code>, ... <code>######</code> will turn that line into a heading.
+    
+    raw -- d.alert.alert-primary
+    raw --   | You can mix regular elements and _Markdown_.
+    raw --   ##### No problem at all.
+    raw --     span.badge.bg-success -- back to elements again
 
-    -- - You can of course use [Markdown Syntax](https://www.markdownguide.org/basic-syntax/) inside these lines. **Bold**, _italics_, [links](#), etc.
-    -- - Unordered lists work too.
-    --   1. As well as ordered ones.
-    --   2. Indentation works the usual way.
-    --   3. Everything should work.
-    --   // - Starting a line with <code>#</code>, <code>##</code>, ... <code>######</code> will turn that line into a heading.
-
-    -- d.alert.alert-primary
-    --   | You can mix regular elements and _Markdown_.
-    --   ##### No problem at all.
-    --     span.badge.bg-success -- back to elements again
-
-  h3
-    -- Stylesheets
+  ### Stylesheets
     s.badge.rounded-pill.bg-warning fs=0.4em -- Vue only
-  p -- Including on the element itself
+  Including on the element itself
 
-  h3 
-    -- Binding to Expressions
+  ### Binding to Expressions
     s.badge.rounded-pill.bg-warning fs=0.4em -- Vue only
 
-  h3
-    -- Special Directives
+  ### Special Directives
     s.badge.rounded-pill.bg-warning fs=0.4em -- Vue only
   
-  h5 > code -- vg-each
-  p -- Vug provides a more concise syntax for loops (compiles down to a regular <code>v-for</code>). You can access the element via the variable <code>it</code>, and the index (or key of an object) via <code>it_i</code>.
+  ##### `vg-each`
+  Vug provides a more concise syntax for loops (compiles down to a regular `v-for`). You can access the element via the variable `it`, and the index (or key of an object) via `it_i`.
   VugDocsExample.mb-4 > pre -- div vg-each=items -- [[[[it.prop]]]]
-  p -- If desired, you can customize the name of the iteration variables using an argument: 
+  If desired, you can customize the name of the iteration variables using an argument: 
   VugDocsExample.mb-4 > pre -- div vg-each:foo=items -- [[[[foo.prop]]]]
 
-  h5 > code -- vg-let
-  p -- Sets a new variable in the current scope. This is useful where you want to refer to it more than once (to be more DRY, or to save computing it twice).
+  ##### `vg-let`
+  Sets a new variable in the current scope. This is useful where you want to refer to it more than once (to be more DRY, or to save computing it twice).
     aside -- (This is accomplished by compiling to a <code>v-for</code> loop over a single-element array.)
   VugDocsExample.mb-4 > pre 
     -- div vg-each=customers
@@ -178,15 +174,15 @@ section.py-4 bg=#AAA
     --     /{{''}}/ More elements here...
     --     .card-body > a -- Send a message to [[[[fullName]]]]
 
-  h5 > code -- vg-do
-  p -- Runs code on the element once, as soon as it is inserted. The element is available as <code>$el</code>.
+  ##### `vg-do`
+  Runs code on the element once, as soon as it is inserted. The element is available as `$el`.
     aside -- (accomplished using Vue's <a href="https://vuejs.org/guide/essentials/template-refs.html#function-refs">Function Refs</a> feature)
   VugDocsExample.mb-4 > pre 
     -- input vg-do=$el.focus() 
     -- div vg-do=customAnimationRoutine($el) 
-  p -- It is sometimes helpful to access global functions in these handlers (i.e. to break out of Vue's sandbox). Vug provides <code>${{''}}win</code> for this:
-    aside -- (accomplished using <code>Array.constructor('return window')()</code>)
-    aside -- (you can do this in any Vug attribute, like in <code>@click</code> handlers)
+  It is sometimes helpful to access global functions in these handlers (i.e. to break out of Vue's sandbox). Vug provides `${{''}}win` for this:
+    aside -- (accomplished using `Array.constructor('return window')()`)
+    aside -- (you can do this in any Vug attribute, like in `@click` handlers)
   VugDocsExample.mb-4 > pre 
     -- input vg-do="${{''}}win.setTimeout(() => $el.focus(), 5000)"
 
