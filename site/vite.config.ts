e821a44f862fea9031bc05b0176ede48c8591b0e
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-08-24 06:54:07 +0300

    try again

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import * as Vug from '../dist/vug.cjs'
 
 // Enable Markdown in Vug
-import { marked } from 'marked'
+import { marked } from '../node_modules/marked/lib/marked.esm' // Not sure why it fails when I just say 'marked'. This was on the GitHub runner: Cannot find module 'marked'
 globalThis.convertMarkdownLine = (text: string) => { const ret: string = marked.parse(text); return ret.slice(3, ret.length - 5) }
 
 // https://vitejs.dev/config/
