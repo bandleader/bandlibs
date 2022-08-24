ed2a82f5157d6c713c3f44379ef1565a088093cb
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-08-24 03:27:08 +0300

    publish site to GitLab Pages

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,5 +4,6 @@ import * as Vug from '../dist/vug.cjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/bandlibs",
   plugins: [Vug.ViteTransformPlugin({_tempLangVersion: 2}), vue()],
 })
