4541c2a420a31a09662401cec717442eacbe1881
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-08-24 11:42:15 +0300

    site vite config use current vug, why not?

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import * as Vug from '../dist/vug.cjs'
+// import * as Vug from '../dist/vug.cjs'
+import * as Vug from '../vug'
 
 // Enable Markdown in Vug
 import { marked } from 'marked'
