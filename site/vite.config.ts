eed97662427b289db9f54edfa58311aecdbd22f4
BL <1201678-bandleader@users.noreply.gitlab.com>
2022-09-21 22:51:42 +0300

    site conditional config

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import * as Vug from '../vug'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/bandlibs",
+export default defineConfig(({command}) => ({ // https://vitejs.dev/config/#conditional-config
+  base: command === 'serve' ? '/' : "/bandlibs/",
   plugins: [Vug.ViteTransformPlugin({_tempLangVersion: 2}), vue()],
-})
+}))
