388608e6b3a0c6d0d78dacc3fd9be39dd25fac3b
BL <1201678-bandleader@users.noreply.gitlab.com>
2023-10-26 01:42:58 -0400

    Revert "move el creation to set. I think better"
    This reverts commit 04dfd3b3a2dc7c171d8f4a56a192a81a6205880a.

--- nof/index.tsx
+++ nof/index.tsx
@@ -38,12 +38,7 @@ function set(el: HTMLElement, key: string, value: any) {
   else if (key[0] === '$') el.style[key.slice(1)] = value // supports both camelCase and kebab-case
   else if (key[0] === '_') el.classList.toggle(key.slice(1).replace(/_/g, '-'), !!value)
   else if (key.startsWith("on")) el.addEventListener(key.slice(2), value)
-  else if (key === "!APPENDCHILD") {
-    const [tag, id] = String(value).split('|')
-    const child = document.createElement(tag)
-    child.setAttribute("_n", id)
-    el.appendChild(child)
-  } else el[key] = value
+  else el[key] = value
 }
 
 function adhocCss(el: HTMLElement, args: string, css: string) {
@@ -225,7 +220,12 @@ class ServerClient {
   doCmd(cmd: Cmd) {
     const el = document.querySelector(`[_n="${cmd.id}"]`) as HTMLElement
     if (!el) throw console.error("Element not found", cmd)
-    set(el, cmd.key, cmd.value)
+    if (cmd.key === "!APPENDCHILD") {
+      const [tag, id] = cmd.value.split('|')
+      const child = document.createElement(tag)
+      child.setAttribute("_n", id)
+      el.appendChild(child)
+    } else set(el, cmd.key, cmd.value)
   }
 }
 
